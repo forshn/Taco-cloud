5d2bd6bec62e17b894720e47cef98b7079e3dfc9
Nikolay <forsh_nikolay@mail.ru>
2022-02-03 23:06:33 +0300

    Junit tests add

--- src/test/java/com/example/tacocloud/TacoCloudApplicationTests.java
+++ src/test/java/com/example/tacocloud/TacoCloudApplicationTests.java
@@ -1,8 +1,16 @@
 package com.example.tacocloud;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)  /*Использование Spring runner
+является аннотацией JUnit, обеспечивая тестовый runner, помогающий JUnit в выполнении теста.
+Думайте об этом как о применении плагина к JUnit для обеспечения пользовательского поведения тестирования.
+В этом случае JUnit получает SpringRunner, тестовый модуль, предоставляемый Spring,
+который обеспечивает создание контекста приложения Spring, с которым будет выполняться тест.
+*/
 @SpringBootTest
 class TacoCloudApplicationTests {
 
