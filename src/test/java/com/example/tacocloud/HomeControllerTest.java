b1e9f6daa295160037e0a463d5e1e584c84c6150
Nikolay <forsh_nikolay@mail.ru>
2022-02-07 17:14:15 +0300

    add Lombok library, create domain models

--- src/test/java/com/example/tacocloud/HomeControllerTest.java
+++ src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -2,12 +2,14 @@ package com.example.tacocloud;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.example.tacocloud.controllers.HomeController;
-import org.junit.Test; import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +21,7 @@ public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;       /*Внедряет MockMvc*/
+
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))              /*Выполняет GET */
@@ -26,5 +29,6 @@ public class HomeControllerTest {
                 .andExpect(view().name("home"))    /*Ожидает home*/
                 .andExpect(content().string( /*Welcome to...*/
 
-                        containsString("Welcome to...")));  }
+                        containsString("Welcome to...")));
+    }
 }
