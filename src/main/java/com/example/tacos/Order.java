111e1b4ad8c913cb5e1e353298eca3484467881a
Nikolay <forsh_nikolay@mail.ru>
2022-02-08 17:45:23 +0300

    Cache properties add, viewController

--- src/main/java/com/example/tacos/Order.java
+++ src/main/java/com/example/tacos/Order.java
@@ -6,9 +6,12 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Date;
 
 @Data
 public class Order {
+    private Long id;
+    private Date placedAt;
     @NotBlank(message = "Должно быть имя")
     private String name;
     @NotBlank(message = "Должно быть название улицы")
